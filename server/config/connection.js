4e246a391156247be9bf234011bd22a10548de80
Arevik-Sunny <arevik.abrahamyan18@gmail.com>
2023-11-18 01:16:51 -0800

    adjusting pages

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks');
 
-module.exports = mongoose.connection;
+const uri = 'mongodb://localhost:27017/googlebooks';
+//Connect to MongoDB
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// Get the default connection
+const connection = mongoose.connection;
+
+connection.on('error', (error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+  
+  connection.once('open', () => {
+    console.log('Connected to MongoDB!');
+  });
+
+  module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ const server = new ApolloServer({
   context: authMiddleware,
 }); //applying my graphql resolvers and typeDefs to make a ApolloServer instance , as well as my auth function
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
